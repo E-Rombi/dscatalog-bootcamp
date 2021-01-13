6913e37df98638f9c1bff8bc05fcf397b9134efe
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-01-13 20:02:40 -0300

    DELETE Category

--- src/main/java/com/eduardo/dscatalog/services/CategoryService.java
+++ src/main/java/com/eduardo/dscatalog/services/CategoryService.java
@@ -3,14 +3,19 @@ package com.eduardo.dscatalog.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eduardo.dscatalog.dto.CategoryDTO;
 import com.eduardo.dscatalog.entities.Category;
 import com.eduardo.dscatalog.repositories.CategoryRepository;
-import com.eduardo.dscatalog.services.exceptions.EntityNotFoundException;
+import com.eduardo.dscatalog.services.exceptions.DatabaseException;
+import com.eduardo.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -35,5 +40,28 @@ public class CategoryService {
 		Category category = CategoryDTO.convert(dto);
 		category = repository.save(category);
 		return new CategoryDTO(category);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category category = repository.getOne(id);
+			category.setName(dto.getName());
+			category = repository.save(category);
+			return new CategoryDTO(category);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 }
--- src/main/java/com/eduardo/dscatalog/services/exceptions/DatabaseException.java
+++ src/main/java/com/eduardo/dscatalog/services/exceptions/DatabaseException.java
@@ -1,10 +1,10 @@
 package com.eduardo.dscatalog.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException {
+public class DatabaseException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public EntityNotFoundException(String message) {
+	public DatabaseException(String message) {
 		super(message);
 	}
 }
