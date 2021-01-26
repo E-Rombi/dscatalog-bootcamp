13ebffe98caa8c4ea9a1ba1a463b89fcad48e573
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-01-26 09:41:59 -0300

    Home Finished

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './core/components/Navbar';
+import Admin from './pages/Admin';
+import Catalog from './pages/Catalog';
+import Home from './pages/Home';
+
+const Routes = () => (
+
+    <BrowserRouter>
+        <Navbar />
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/catalog" component={Catalog} />
+            <Route path="/admin" component={Admin} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Routes;
