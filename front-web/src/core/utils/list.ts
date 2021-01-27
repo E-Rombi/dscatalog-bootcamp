ffcf0d949a176aeb83e9c05a7cb4e48a0785c26a
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-01-27 14:33:35 -0300

    Add Loaders

---
+++ front-web/src/core/utils/list.ts
@@ -0,0 +1,3 @@
+export const generateList = (items: number) => {
+    return Array.from(Array(items).keys());
+}
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductsResponse } from '../../core/types/Product';
+import { ProductsResponse } from 'core/types/Product';
 import { makeRequest } from '../../core/utils/request';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const params = {
-            page:0,
+            page: 0,
             linesPerPage: 30
         }
 
+        setIsLoading(true);
+
         makeRequest({ url: '/products', params })
             .then(resp => setProductsResponse(resp.data))
-            .catch(error => console.log(error));
+            .catch(error => console.log(error))
+            .finally(() => {
+                setIsLoading(false);
+            });
 
     }, []);
 
@@ -24,13 +31,15 @@ const Catalog = () => {
         <div className="catalog-container">
             <h1 className="catalog-title">Catálogo de Produtos</h1>
             <div className="catalog-products">
-                {
-                    productsResponse?.content.map( product => (
+                {isLoading ? <ProductCardLoader /> : (
+                    productsResponse?.content.map(product => (
                         <Link key={product.id} to={`/products/${product.id}`}>
-                            <ProductCard product={product}/>
+                            <ProductCard product={product} />
                         </Link>
                     ))
-                }
+
+                )}
+
             </div>
         </div>
     );
