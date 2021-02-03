103c3f9188f9b67b14993bf60d58a09f262d6fef
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-02-03 19:05:59 -0300

    Add Pagination

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,15 +5,17 @@ import { makeRequest } from '../../core/utils/request';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
+import Pagination from 'core/components/Pagination';
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         const params = {
-            page: 0,
-            linesPerPage: 30
+            page: activePage,
+            linesPerPage: 12
         }
 
         setIsLoading(true);
@@ -25,7 +27,7 @@ const Catalog = () => {
                 setIsLoading(false);
             });
 
-    }, []);
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -41,6 +43,10 @@ const Catalog = () => {
                 )}
 
             </div>
+            { productsResponse && <Pagination  
+                                        totalPages={productsResponse.totalPages} 
+                                        activePage={activePage}
+                                        onChange={page => setActivePage(page)} />}
         </div>
     );
 }
