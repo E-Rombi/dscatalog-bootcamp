8cf0e8895e82757e12f3dcca9796cbb45eff8650
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-02-19 19:08:49 -0300

    First upload to S3

--- backend/src/main/java/com/eduardo/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/eduardo/dscatalog/DscatalogApplication.java
@@ -1,12 +1,24 @@
 package com.eduardo.dscatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DscatalogApplication {
+import com.eduardo.dscatalog.services.S3Service;
 
+@SpringBootApplication
+public class DscatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("c:\\temp\\teste.jpg");
 	}
 }
--- backend/src/main/java/com/eduardo/dscatalog/repositories/RoleRepository.java
+++ backend/src/main/java/com/eduardo/dscatalog/repositories/RoleRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.eduardo.dscatalog.entities.Role;
-import com.eduardo.dscatalog.entities.User;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
