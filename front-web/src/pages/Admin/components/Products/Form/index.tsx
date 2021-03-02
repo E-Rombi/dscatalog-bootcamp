3070fa8c77f827a24cef1b617a237a541449a564
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-03-02 20:16:58 -0300

    Implemented React Hook Form in ProductForm

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,76 +1,62 @@
-import React, { useState } from 'react';
-import { makePrivateRequest } from 'core/utils/request';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
+import { useForm } from 'react-hook-form';
+import { makePrivateRequest } from 'core/utils/request';
 
 type FormState = {
     name: string;
     price: string;
-    category: string;
     description: string;
+    imageUrl: string;
 }
 
-type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
-
 const Form = () => {
-    const [formData, setFormData] = useState<FormState>({
-        name: '',
-        price: '',
-        category: '3',
-        description: ''
-    });
+    const { register, handleSubmit } = useForm<FormState>();
 
-    const handleOnChange = (event: FormEvent) => {
-        const name = event.target.name;
-        const value = event.target.value;
-
-        setFormData(data => ({...data, [name]: value}));
-    }
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const payload = {
-            ...formData,
-            imgUrl: 'https://images5.kabum.com.br/produtos/fotos/128375/placa-de-video-evga-nvidia-geforce-rtx-3090-24gb-gddr6x-24g-p5-3975-kr_1600783698_g.jpg',
-            categories: [{id: formData.category}]
-        }
-        
-        makePrivateRequest({url: '/products', method:'POST', data: payload})
+    const onSubmit = (data: FormState) => {
+        makePrivateRequest({url: '/products', method:'POST', data});
     }
 
     return (
-        <form action="" onSubmit={handleSubmit}>
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
             <BaseForm title="CADASTRAR PRODUTO">
                 <div className="row">
                     <div className="col-6">
                         <input
+                            ref={register({required: "Campo obrigatório"})}
                             type="text" 
-                            value={formData.name}
                             name="name"
-                            className="form-control my-5" 
-                            onChange={handleOnChange} 
+                            className="form-control input-base margin-bottom-30"  
                             placeholder="Nome do Produto" 
                         />
-                        <select value={formData.category} className="form-control mb-5" onChange={handleOnChange} name="category">
-                            <option value="1">Livros</option>
-                            <option value="2">Eletrônicos</option>
-                            <option value="3">Computadores</option>
-                        </select>
+                        
                         <input 
-                            type="text"
-                            value={formData.price} 
+                            type="number"
+                            ref={register({required: "Campo obrigatório"})}
                             name="price"
-                            className="form-control" 
-                            onChange={handleOnChange} 
+                            className="form-control input-base margin-bottom-30" 
                             placeholder="Preço" 
                         />
+
+                        <input
+                            type="text" 
+                            ref={register({required: "Campo obrigatório"})}
+                            name="imageUrl"
+                            className="form-control input-base margin-bottom-30" 
+                            placeholder="Imagem do Produto" 
+                        />
+
                     </div>
                     <div className="col-6">
                         <textarea 
                             name="description" 
-                            className="form-control my-5" 
-                            cols={30} rows={8}
-                            onChange={handleOnChange}></textarea>
+                            ref={register({required: "Campo obrigatório"})}
+                            placeholder="Descrição"
+                            className="form-control input-base" 
+                            cols={30} rows={10}
+                        >
+                            
+                        </textarea>
                     </div>
 
                 </div>
